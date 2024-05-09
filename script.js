a03000868a0369801086681cb4a40d8b8c237abe
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-09 02:06:53 +0000

    [update]8章

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper',{
+  loot:true,
+  
+  pagination: {
+    el:'.swiper-pagination',
+  },
+  
+  navigation: {
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev',
+  }
+});
